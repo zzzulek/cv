921e9c2d079125ccb474fd10c861b082bfff1285
Zulfiya Kurbanova <kazulek96@gmail.com>
2019-12-01 02:33:08 +0000

    add flickity

--- public/js/app.js
+++ public/js/app.js
@@ -10733,4 +10733,60 @@ var cursor = {
 
 cursor.init();
 
+
+
+// Play with this value to change the speed
+let tickerSpeed = 2;
+
+let flickity = null;
+let isPaused = false;
+const slideshowEl = document.querySelector('.js-slideshow');
+
+const update = () => {
+  if (isPaused) return;
+  if (flickity.slides) {
+    flickity.x = (flickity.x - tickerSpeed) % flickity.slideableWidth;
+    flickity.selectedIndex = flickity.dragEndRestingSelect();
+    flickity.updateSelectedSlide();
+    flickity.settle(flickity.x);
+  }
+  window.requestAnimationFrame(update);
+};
+
+const pause = () => {
+  isPaused = true;
+};
+
+const play = () => {
+  if (isPaused) {
+    isPaused = false;
+    window.requestAnimationFrame(update);
+  }
+};
+
+
+flickity = new Flickity(slideshowEl, {
+  autoPlay: false,
+  prevNextButtons: false,
+  pageDots: false,
+  draggable: true,
+  wrapAround: true,
+  selectedAttraction: 0.015,
+  friction: 0.25
+});
+flickity.x = 0;
+
+
+slideshowEl.addEventListener('mouseenter', pause, false);
+slideshowEl.addEventListener('focusin', pause, false);
+slideshowEl.addEventListener('mouseleave', play, false);
+slideshowEl.addEventListener('focusout', play, false);
+
+flickity.on('dragStart', () => {
+  isPaused = true;
+});
+
+
+update();
+
 //# sourceMappingURL=app.js.map
