c1ed3ccf4485ddc5391df0b400528f1cb664bf92
Zulfiya Kurbanova <kazulek96@gmail.com>
2019-12-03 13:39:40 +0000

    swiper on projects block

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ elixir(function(mix) {
 
     mix.less([
         './node_modules/normalize.less/normalize.less',
+        './node_modules/swiper/dist/css/swiper.css',
         './assets/less/style.less',
         // './assets/less/core/*.less',
         // './assets/less/blocks/*.less',
@@ -12,6 +13,7 @@ elixir(function(mix) {
 
     mix.scripts([
         './node_modules/jquery/dist/jquery.js',
+        './node_modules/swiper/dist/js/swiper.js',
         './assets/js/app.js',
     ], 'public/js/app.js');
 
